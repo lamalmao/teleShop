6714f72ab7f7aaa690a05ed297a355e97d99ff26
lamalmao <laesia>
2022-11-18 13:53:36 +0300

    security update

--- payment_service/index.js
+++ payment_service/index.js
@@ -3,11 +3,26 @@ const express = require('express');
 const users = require('../models/users');
 const payments = require('../models/payments');
 
+const allowedIPs = [
+  '185.162.128.38', 
+  '185.162.128.39', 
+  '185.162.128.88',
+  '127.0.0.1'
+];
+
 function createPaymentProvider(bot) {
   const app = express();
 
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+
+  app.use('/payment', async (req, res, next) => {
+    if (allowedIPs.includes(req.socket.remoteAddress)) next();
+    else {
+      console.log(`Попытка проведения платежа с ${res.socket.remoteAddress}`);
+      res.status(401).end('No access');
+    }
+  });
 
   app.post('/payment', async (req, res) => {
     try {
