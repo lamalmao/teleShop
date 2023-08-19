e903c451eabbd339c5ec88fddeca85dab51d7b35
lamalmao <114337080+lamalmao@users.noreply.github.com>
2023-08-19 16:45:25 +0300

    Намана работает

--- bot/scenes/profile.js
+++ bot/scenes/profile.js
@@ -82,9 +82,7 @@ profile.on('message', async ctx => {
     if (ctx.scene.state.action === 'refill') {
       let amount = Number(ctx.message.text.trim());
 
-      // && amount >= 100 && amount <= 10000
-
-      if (!Number.isNaN(amount) ) {
+      if (!Number.isNaN(amount) && amount >= 100 && amount <= 10000) {
         amount = amount.toFixed(2);
         ctx.scene.state.action = undefined;
         ctx.scene.enter('pay', { menu: ctx.scene.state.menu, amount: amount });
